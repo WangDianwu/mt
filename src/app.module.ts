530444714c7322377df3637b5295f7626a088742
dianwuwang <wangdianwu@citicpub.com>
2024-03-13 11:09:03 +0800

    add iamges

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
+/*
+ * @Description:
+ * @Version: 1.0
+ * @Author: WangDianWu
+ * @Date: 2024-03-08 15:53:25
+ * @LastEditors: WangDianWu
+ * @LastEditTime: 2024-03-12 17:11:15
+ */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'), // 静态文件目录
+      serveRoot: '/public', // 访问静态文件的URL路径
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,31 @@
  * @Author: WangDianWu
  * @Date: 2024-03-08 15:53:25
  * @LastEditors: WangDianWu
- * @LastEditTime: 2024-03-12 09:45:29
+ * @LastEditTime: 2024-03-12 11:43:50
  */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { getDataSource } from './data-source';
+// import { getDataSource } from './data-source';
+// import * as serveStatic from 'serve-static';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
-require("reflect-metadata")
+// import express from 'express';
+
+// require("reflect-metadata")
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // app.use('/public', express.static(path.join(__dirname, '../public')));
+  app.enableCors();
+  app.useStaticAssets(join(__dirname, './', 'public'));
+  // app.use(
+  //   '/public',
+  //   serveStatic(path.join(__dirname, '../public'), {
+  //     maxAge: '1d',
+  //     extensions: ['jpg', 'jpeg', 'png', 'gif'],
+  //   }),
+  // );
   await app.listen(3000);
 }
 bootstrap();
